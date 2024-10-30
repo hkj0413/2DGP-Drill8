d2f37ae385afc5c43befec942b0b3b2acc328b82
hkj0413 <60205654+hkj0413@users.noreply.github.com>
2024-10-30 16:30:29 +0900

    파일 생성

---
+++ boy.py
@@ -0,0 +1,100 @@
+from pico2d import load_image, get_time
+
+from state_machine import StateMachine, space_down, time_out, right_down, right_up, left_down, left_up
+
+class Idle:
+    @staticmethod
+    def enter(boy, e):
+        if left_up(e) or right_down(e):
+            boy.action = 2
+        elif right_up(e) or left_down(e):
+            boy.action = 3
+
+        boy.dir = 0
+        boy.frame = 0
+        boy.start_time = get_time()
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        if get_time() - boy.start_time > 5:
+            boy.state_machine.add_event(('TIME_OUT', 0))
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
+class Sleep:
+    @staticmethod
+    def enter(boy, e):
+        pass
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+    @staticmethod
+    def draw(boy):
+        if boy.action == 3:
+            boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100, 3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
+        elif boy.action == 2:
+            boy.image.clip_composite_draw(boy.frame * 100, boy.action * 100, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
+
+class Run:
+    @staticmethod
+    def enter(boy, e):
+        if right_down(e) or left_up(e):
+            boy.dir = 1
+            boy.action = 1
+        elif left_down(e) or right_up(e):
+            boy.dir = -1
+            boy.action = 0
+
+        boy.frame = 0
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 5
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.dir = 0
+        self.action = 3
+        self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start(Idle)
+        self.state_machine.set_transitions(
+            {
+                Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
+                Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
+                Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle}
+            }
+        )
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        self.state_machine.add_event( ('INPUT', event) )
+
+    def draw(self):
+        self.state_machine.draw()
+
---
+++ state_machine.py
@@ -0,0 +1,48 @@
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDL_KEYUP, SDLK_RIGHT, SDLK_LEFT
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+class StateMachine:
+    def __init__(self, o):
+        self.o = o
+        self.event_que = []
+
+    def start(self, start_state):
+        self.cur_state = start_state
+        self.cur_state.enter(self.o, ('START', 0))
+
+    def update(self):
+        self.cur_state.do(self.o)
+        if self.event_que:
+            e = self.event_que.pop(0)
+            for check_event, next_state in self.transitions[self.cur_state].items():
+                if check_event(e):
+                    self.cur_state.exit(self.o, e)
+                    self.cur_state = next_state
+                    self.cur_state.enter(self.o, e)
+                    return
+
+    def draw(self):
+        self.cur_state.draw(self.o)
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
+
+    def add_event(self, e):
+        self.event_que.append(e)
